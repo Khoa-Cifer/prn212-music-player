78175fb60c52d2fc018e8649bea4b520126f2a61
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-10-23 09:59:51 +0700

    update mp3 player

--- Music Player/Music Player/MainWindow.xaml.cs
+++ Music Player/Music Player/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Music_Player.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Music_Player
@@ -6,14 +8,20 @@ namespace Music_Player
 
     public partial class MainWindow : Window
     {
+        private SongViewModel _songViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
+            _songViewModel = new SongViewModel();
+            this.DataContext = _songViewModel;
+
+            _songViewModel.PlaySongAction = (filePath) =>
+            {
+                mediaElement.Source = new Uri(filePath, UriKind.RelativeOrAbsolute);
+                mediaElement.Play();
+            };
         }
     }
 }
