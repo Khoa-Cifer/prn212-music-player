f7bb971b91f250cb217cf60d48b68a39fe1ea35b
Danh181 <danhthanh18102004@gmail.com>
2024-10-23 22:41:50 +0700

    init code

---
+++ Music Player/Music Player/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Music_Player
+{
+
+    public partial class MainWindow : Window
+    {
+        string[] paths, files;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void QuitButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult answer = MessageBox.Show("Do you really want to quit??", "Quit", MessageBoxButton.YesNo, MessageBoxImage.Error);
+            if (answer == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+        private void GODS_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Music Player/Music Player/UserControlls/Playlist.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Music_Player.UserControlls
+{
+    public partial class Playlist : UserControl
+    {
+        public Playlist()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Playlist));
+
+        public string Desc
+        {
+            get { return (string)GetValue(DescProperty); }
+            set { SetValue(DescProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescProperty = DependencyProperty.Register("Desc", typeof(string), typeof(Playlist));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(Playlist));
+
+        public bool isActive
+        {
+            get { return (bool)GetValue(isActiveProperty); }
+            set { SetValue(isActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty isActiveProperty = DependencyProperty.Register("isActive", typeof(bool), typeof(Playlist));
+    }
+}
