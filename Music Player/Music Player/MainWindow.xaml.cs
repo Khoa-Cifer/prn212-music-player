e64c37bb038c9227c779bf883f3db445ecbf6804
hoangnd <hoangndse184443@fpt.edu.vn>
2024-10-28 17:13:22 +0700

    update ui

--- Music Player/Music Player/MainWindow.xaml.cs
+++ Music Player/Music Player/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MahApps.Metro.IconPacks;
 using Music_Player.ViewModel;
+<<<<<<< Updated upstream
 using System.Media;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,10 +12,25 @@ using Microsoft.WindowsAPICodePack.Dialogs;
 namespace Music_Player
 {
 
+=======
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows.Threading;
+
+namespace Music_Player
+{
+>>>>>>> Stashed changes
     public partial class MainWindow : Window
     {
         private bool isPlaying = true;
         private SongViewModel _songViewModel;
+<<<<<<< Updated upstream
+=======
+        private DispatcherTimer _timer;
+>>>>>>> Stashed changes
 
         public MainWindow()
         {
@@ -23,7 +39,11 @@ namespace Music_Player
             CommonOpenFileDialog dialog = new CommonOpenFileDialog
             {
                 InitialDirectory = @"C:\Users",
+<<<<<<< Updated upstream
                 IsFolderPicker = true 
+=======
+                IsFolderPicker = true
+>>>>>>> Stashed changes
             };
 
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
@@ -34,10 +54,42 @@ namespace Music_Player
             {
                 _songViewModel = new SongViewModel(@"C:\Users\Cifer\Music");
             }
+<<<<<<< Updated upstream
+=======
+
+            InitializeMediaTimer();
+        }
+
+        private void InitializeMediaTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += UpdateSeekBar;
+        }
+
+        private void UpdateSeekBar(object sender, EventArgs e)
+        {
+            if (mediaElement.NaturalDuration.HasTimeSpan)
+            {
+                TimeSpan duration = mediaElement.NaturalDuration.TimeSpan;
+                TotalDurationText.Text = duration.ToString(@"mm\:ss");
+
+                TimeSpan currentTime = mediaElement.Position;
+                CurrentTimeText.Text = currentTime.ToString(@"mm\:ss");
+
+                SeekBar.Maximum = duration.TotalSeconds;
+                SeekBar.Value = currentTime.TotalSeconds;
+            }
+>>>>>>> Stashed changes
         }
 
         private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
         {
+<<<<<<< Updated upstream
+=======
+            _timer.Stop(); // Stop the timer when the song ends
+
+>>>>>>> Stashed changes
             if (_songViewModel != null && _songViewModel.NextSongCommand.CanExecute(null))
             {
                 _songViewModel.NextSongCommand.Execute(null);
@@ -46,7 +98,11 @@ namespace Music_Player
 
         private void quitButton_Click(object sender, RoutedEventArgs e)
         {
+<<<<<<< Updated upstream
             MessageBoxResult answer = MessageBox.Show("Do you really want to quit??", "Quit", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+=======
+            MessageBoxResult answer = MessageBox.Show("Do you really want to quit?", "Quit", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+>>>>>>> Stashed changes
             if (answer == MessageBoxResult.Yes)
             {
                 Application.Current.Shutdown();
@@ -60,14 +116,25 @@ namespace Music_Player
                 mediaElement.Pause();
                 isPlaying = false;
                 playPauseIcon.Kind = PackIconMaterialKind.Play;
+<<<<<<< Updated upstream
+=======
+                _timer.Stop(); // Stop updating the SeekBar
+>>>>>>> Stashed changes
             }
             else
             {
                 mediaElement.Play();
                 isPlaying = true;
                 playPauseIcon.Kind = PackIconMaterialKind.Pause;
+<<<<<<< Updated upstream
             }
         }
+=======
+                _timer.Start(); // Start updating the SeekBar
+            }
+        }
+
+>>>>>>> Stashed changes
         private void slider_VolumeChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             if (mediaElement != null)
@@ -76,6 +143,25 @@ namespace Music_Player
             }
         }
 
+<<<<<<< Updated upstream
+=======
+        private void SeekButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (mediaElement.NaturalDuration.HasTimeSpan)
+            {
+                TimeSpan newPosition = mediaElement.Position + TimeSpan.FromSeconds(10);
+                mediaElement.Position = newPosition < mediaElement.NaturalDuration.TimeSpan ? newPosition : mediaElement.NaturalDuration.TimeSpan;
+            }
+        }
+
+        private void SeekBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (Math.Abs(e.NewValue - mediaElement.Position.TotalSeconds) > 1)
+            {
+                mediaElement.Position = TimeSpan.FromSeconds(e.NewValue);
+            }
+        }
+>>>>>>> Stashed changes
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
@@ -95,9 +181,17 @@ namespace Music_Player
 
         private void SelectFolder_Click(object sender, RoutedEventArgs e)
         {
+<<<<<<< Updated upstream
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
             dialog.InitialDirectory = "C:\\Users";
             dialog.IsFolderPicker = true;
+=======
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog
+            {
+                InitialDirectory = "C:\\Users",
+                IsFolderPicker = true
+            };
+>>>>>>> Stashed changes
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 MessageBox.Show("You selected: " + dialog.FileName);
@@ -109,6 +203,13 @@ namespace Music_Player
                     mediaElement.Source = new Uri(filePath, UriKind.RelativeOrAbsolute);
                     mediaElement.Play();
                     mediaElement.MediaEnded += MediaElement_MediaEnded;
+<<<<<<< Updated upstream
+=======
+                    TotalDurationText.Text = mediaElement.NaturalDuration.HasTimeSpan
+                                              ? mediaElement.NaturalDuration.TimeSpan.ToString(@"mm\:ss")
+                                              : "0:00";
+                    _timer.Start(); // Start timer for the new song
+>>>>>>> Stashed changes
                 };
             }
         }
@@ -118,4 +219,8 @@ namespace Music_Player
             _songViewModel.ShowVideoView();
         }
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
